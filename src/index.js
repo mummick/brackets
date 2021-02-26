71ee1f9a2155eac059c43ced0bba4bffbed7bbfb
mummick <mummick6@gmail.com>
2021-02-26 21:17:48 +0300

    feat: solve 'brackets' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let openBrackets = [];
+    let closeBrackets = [];
+    let stackArray = [];
+
+    bracketsConfig.forEach((element) => {
+        openBrackets.push(element[0]);
+        closeBrackets.push(element[1]);
+    });
+
+    let last = "";
+    let index = -1;
+    for (let bracket of str) {
+        last = stackArray.pop();
+        if ((index = closeBrackets.indexOf(bracket)) != -1) {
+            if (last === openBrackets[index]) {
+                //do nothing
+            } else {
+                if (openBrackets[index] === closeBrackets[index]) {
+                    if (last != undefined) {
+                        stackArray.push(last);
+                    }
+                    stackArray.push(bracket);
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            if (last != undefined) {
+                stackArray.push(last);
+            }
+            stackArray.push(bracket);
+        }
+    }
+    return stackArray.length == 0;
+};
